2872af0ac809293faf905a95dbd6823a31e2ec4f
PJBackStack <majnightowl@gmail.com>
2025-06-21 20:36:22 +0200

    Small changes to data models

--- lib.js
+++ lib.js
@@ -6,7 +6,7 @@ class Character {
     constructor({
                     name,
                     level = 1,
-                    maxHP = 100,
+                    maxHealth = 100,
                     maxResource = 50,
                     strength = 10,
                     magic = 10,
@@ -16,8 +16,8 @@ class Character {
                 }) {
         this.name = name;
         this.level = level;
-        this.maxHP = maxHP;
-        this.hp = maxHP;
+        this.maxHealth = maxHealth
+        this.health = maxHealth;
         this.maxResource = maxResource;
         this.resource = maxResource;
 
@@ -29,7 +29,19 @@ class Character {
         this.statusEffects = []; // array of status objects like stun, burn, etc.
     }
 
+    adjustHealth(amount = 0) {
+        this.health += amount;
+        if (this.health > this.maxHealth) {
+            this.health = this.maxHealth;
+        }
+    }
 
+    adjustResource(amount = 0) {
+        this.resource += amount;
+        if (this.resource > this.maxResource) {
+            this.resource = this.maxResource;
+        }
+    }
 
     isAlive() {
         return this.hp > 0;
@@ -43,11 +55,27 @@ class Character {
 class Player extends Character {
     constructor(config) {
         super(config);
+        this.experience = 0;
+        this.requiredExperience = 100;
         this.isPlayer = true;
 
-        this.inventory = {}
+        this.inventory = {};
+        this.equipment = {};
         // Should look like:
         // potion: { quantity: 1, use() => {w/e} }, just check against itemList for static info
+    }
+
+    addExperience(experience = 0) {
+        this.experience += experience;
+        while (this.experience >= this.requiredExperience) {
+            this.levelUp()
+        }
+    }
+
+    levelUp() {
+        this.experience -= this.requiredExperience;
+        this.requiredExperience = Math.round(1.21 * this.requiredExperience);
+        this.level++;
     }
 
     addItemToInventory(key, quantity = 1) {
@@ -68,7 +96,7 @@ class Player extends Character {
                         equipItem(this.name);
                     }
                 })
-            };
+            }
         } else {
             inventory[key].quantity += quantity;
         }
